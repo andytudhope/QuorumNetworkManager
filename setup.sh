1f5351309cd8ec2d36d7dc61b5307b242a62f4d3
Andy Tudhope <tuddy0525@gmail.com>
2017-03-16 16:25:51 +0200

    fix for nvm `source` issues

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@
 sudo apt-get update;
 sudo apt-get install -y build-essential libssl-dev;
 
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh;
-bash install_nvm.sh;
-source ~/.profile;
-nvm install 4.8.0;
-nvm use 4.8.0;
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 4.8.0
+nvm use 4.8.0
 
 wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz;
 sudo tar -xvf go1.7.linux-amd64.tar.gz;
@@ -18,10 +18,11 @@ export GOROOT=/usr/local/go;
 export GOPATH=$HOME/projects/go;
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH;
 
-sudo apt-get install -y software-properties-common;
-sudo add-apt-repository -y ppa:ethereum/ethereum;
-sudo apt-get update;
-sudo apt-get install -y ethereum;
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install -y ethereum
+sudo mv /usr/bin/geth /usr/bin/normalGeth
 
 git clone https://github.com/jpmorganchase/quorum.git;
 cd quorum/;
